70338ee968972fd74f5e58f9c911a53e329915bb
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-12-19 23:20:28 +0100

    Refactor settings to show header footer and SEO in different tabs, remove notFound, add descriptions

--- server/sanity/schemas/documents/settings.ts
+++ server/sanity/schemas/documents/settings.ts
@@ -1,20 +1,20 @@
 import {CogIcon} from '@sanity/icons'
-import {defineField, defineType} from 'sanity'
+import {defineType} from 'sanity'
 
 export default defineType({
   name: 'settings',
-  title: 'Settings',
+  title: 'Settings and navigation',
   type: 'document',
   icon: CogIcon,
   groups: [
     {
       default: true,
-      name: 'navigation',
-      title: 'Navigation',
+      name: 'header',
+      title: 'Header',
     },
     {
-      name: 'notFoundPage',
-      title: '404 page',
+      name: 'footer',
+      title: 'Footer',
     },
     {
       name: 'seo',
@@ -22,54 +22,85 @@ export default defineType({
     },
   ],
   fields: [
-    // Header
-    defineField({
+    {
       name: 'header',
       title: 'Header',
       type: 'object',
-      group: 'navigation',
-      fields: [
-        defineField({
-          name: 'title',
-          title: 'Title',
-          type: 'string',
-        }),
-        defineField({
-          name: 'content',
-          title: 'Content',
-          type: 'text',
-        }),
-      ],
-    }),
+      group: 'header',
+      validation: (Rule) => Rule.required(),
 
-    defineField({
+      fields: [
+        {
+          name: 'logo',
+          title: 'Logo',
+          type: 'image',
+          description:
+            'Upload your company logo here. It will be displayed in the header across the site.',
+          validation: (Rule) => Rule.required(),
+        },
+        {
+          name: 'navLinks',
+          title: 'Navigation Links',
+          type: 'array',
+          of: [{type: 'link'}],
+          description:
+            'Define the links for the site navigation. Each link should direct users to a different section or page.',
+          validation: (Rule) => Rule.required().min(1),
+        },
+        {
+          name: 'chartIcon',
+          title: 'Cart icon',
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+          description:
+            'Upload an icon for the shopping cart. This will be used in the header to represent the cart.',
+        },
+      ],
+    },
+
+    {
       name: 'footer',
       title: 'Footer',
-      type: 'string',
-      // group: 'navigation',
-    }),
+      type: 'object',
+      group: 'footer',
+      validation: (Rule) => Rule.required(),
+      description:
+        'Add different sections to the footer, such as contact information, address, navigation links, socials or any additional info about your business.',
+
+      fields: [
+        {
+          name: 'sections',
+          title: 'Sections',
+          type: 'array',
+          of: [{type: 'footerSection'}],
+          validation: (Rule) => Rule.required().min(1).max(4),
+        },
+      ],
+    },
+
+    {
+      name: 'seo',
+      title: 'SEO Settings',
+      type: 'object',
+      group: 'seo',
+      fields: [
+        {
+          name: 'metaTitle',
+          title: 'Meta Title',
+          type: 'string',
+          description:
+            'This is the title of your site as it will appear in search engines. Keep it concise but descriptive.',
+        },
+        {
+          name: 'metaDescription',
+          title: 'Meta Description',
+          type: 'text',
+          description:
+            'This description will appear in search engine results. Summarize the content of the site and include relevant keywords to improve search rankings.',
+        },
+      ],
+    },
   ],
 })
-
-// Not found page
-//   defineField({
-//     name: 'notFoundPage',
-//     title: '404 page',
-//     type: 'notFoundPage',
-//     group: 'notFoundPage',
-//   }),
-//   // SEO
-//   defineField({
-//     name: 'seo',
-//     title: 'SEO',
-//     type: 'seo',
-//     group: 'seo',
-//   }),
-// ],
-// preview: {
-//   prepare() {
-//     return {
-//       title: TITLE,
-//     }
-//   },
-// },
