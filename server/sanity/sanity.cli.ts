23987c7d5e6d2fa6138e21d7c96456d556a61519
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-12-20 16:46:53 +0100

    Define singleton documents + fix spelling error

--- server/sanity/sanity.cli.ts
+++ server/sanity/sanity.cli.ts
@@ -1,8 +1,8 @@
-import { defineCliConfig } from 'sanity/cli'
+import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_PROEJCTID,
+    projectId: process.env.SANITY_PROJECTID,
     dataset: 'production',
   },
 })
