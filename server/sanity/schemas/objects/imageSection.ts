fd14b9e6a6fa46238637a1f8d10be44cec2de88c
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-01-01 18:24:24 +0100

    Update validation and add hotspot to image

--- server/sanity/schemas/objects/imageSection.ts
+++ server/sanity/schemas/objects/imageSection.ts
@@ -14,16 +14,21 @@ export default defineType({
     defineField({
       name: 'imageCards',
       title: 'Image cards',
+      description: 'This is a section to display images with links to other parts of the page',
       type: 'array',
+      validation: (Rule) => Rule.length(3).warning('Add three image cards to this section'),
       of: [
         {
           type: 'object',
           fields: [
-            {
+            defineField({
               name: 'image',
               title: 'Image',
               type: 'image',
-            },
+              options: {
+                hotspot: true,
+              },
+            }),
             {
               name: 'alt',
               title: 'Alt Text',
