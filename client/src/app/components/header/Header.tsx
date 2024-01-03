9409cd7b88b90f5c6eac8803b945b414f96684db
Linus Hernvall <linus.hernvall@gmail.com>
2024-01-03 09:42:31 +0100

    Added postcss.config.js.

--- client/src/app/components/header/Header.tsx
+++ client/src/app/components/header/Header.tsx
@@ -1,10 +1,13 @@
+'use client';
 import { Box, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 import logotype from '../../../../public/images/bellis-logo.png';
 import CartTag from '../cartTag/CartTag';
 import classes from './Header.module.css';
 
 export default function Header() {
+  const [opened, { toggle }] = useDisclosure();
   return (
     <Box className={classes.container}>
       <img
@@ -13,7 +16,12 @@ export default function Header() {
       />
       <Box className={classes.content}>
         <CartTag />
-        <Burger className={classes.burger} aria-label="Toggle navigation" />
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          className={classes.burger}
+          aria-label="Toggle navigation"
+        />
       </Box>
     </Box>
   );
