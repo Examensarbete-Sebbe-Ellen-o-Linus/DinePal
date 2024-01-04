4096a5b7abbee7afaf0ecb6a3a663a48bc086b05
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-01-04 10:34:44 +0100

    Create news component, not styled yet

---
+++ src/app/_components/news/News.tsx
@@ -0,0 +1,15 @@
+import { Box, Container, Image, Text, Title } from '@mantine/core';
+import type { INews } from '~/app/interfaces';
+import classes from './News.module.scss';
+
+export default async function News({ news }: { news: INews }) {
+  return (
+    <Container className={classes.container}>
+      <Box>
+        <Title order={3}>{news.title}</Title>
+        <Text>{news.description}</Text>
+      </Box>
+      <Image src={news.image.url} alt={news.image.alt}></Image>
+    </Container>
+  );
+}
