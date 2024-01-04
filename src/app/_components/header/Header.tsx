62f6706f111af8a2ae429b06b99d9b67102cfa79
Linus Hernvall <linus.hernvall@gmail.com>
2024-01-04 14:34:40 +0100

    Hide/ show header on scroll

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -4,6 +4,7 @@ import { useDisclosure, useMediaQuery } from '@mantine/hooks';
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { theme } from '~/app/theme/theme';
 import logotype from '../../../../public/images/bellis-logo.png';
 import CartTag from '../cartTag/CartTag';
@@ -12,8 +13,29 @@ import classes from './Header.module.css';
 export default function Header() {
   const [opened, { toggle }] = useDisclosure();
   const isDesktop = useMediaQuery(`(min-width: ${theme.breakpoints?.sm})`);
+  const [lastScrollUp, setLastScrollUp] = useState(0);
+  const [headerVisible, setHeaderVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollUp = window.scrollY || document.documentElement.scrollTop;
+      if (scrollUp > lastScrollUp) {
+        setHeaderVisible(false);
+      } else {
+        setHeaderVisible(true);
+      }
+      setLastScrollUp(scrollUp <= 0 ? 0 : scrollUp);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollUp]);
   return (
-    <Box className={classes.container}>
+    <Box
+      className={`${classes.container} ${!headerVisible ? classes.hide : ''}`}
+    >
       <Link href='/'>
         <Image
           src={logotype}
