712111e4e1bd2a3244cf338906c6ace469c75632
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-01-08 11:21:25 +0100

    Update about link and remove gallery link from startpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,6 @@ export default async function Home() {
 
   return (
     <div>
-      <Link href='/gallery'>Link to Gallery</Link>
       <Hero hero={hero} />
       <SelectedDishes dishes={selectedDishes} />
       <News news={news} />
