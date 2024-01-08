f23aa35bdfa8f23d983c8d17bcf7ae60f2ff6cca
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-01-08 10:08:30 +0100

    Change from css to scss

--- src/app/_components/about/About.tsx
+++ src/app/_components/about/About.tsx
@@ -1,12 +1,17 @@
 import { Container } from '@mantine/core';
 import { IAbout } from '~/app/interfaces';
+import scss from './About.module.scss';
 
 export default async function About({ about }: { about: IAbout }) {
   return (
-    <section className='container'>
+    <section className={scss.container}>
       <Container>
-        <h2>{about.title}</h2>
+        {/* {about.image ?? <img src={about.image.url} alt={about.image.alt} />}
+        <Title order={3}>{about.title}</Title>
         {about.description && <p>{about.description}</p>}
+        {about.links?.map(link) => (
+          <Link src={about.}></Link>
+        )} */}
       </Container>
     </section>
   );
