26accb4290df3e62d2426b2c270737aa6925c7ac
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-01-08 11:22:40 +0100

    Update content and style in about section

--- src/app/_components/about/About.tsx
+++ src/app/_components/about/About.tsx
@@ -1,17 +1,21 @@
-import { Container } from '@mantine/core';
-import { IAbout } from '~/app/interfaces';
+import { Box, Container, Text, Title } from '@mantine/core';
+import type { IAbout } from '~/app/interfaces';
 import scss from './About.module.scss';
 
 export default async function About({ about }: { about: IAbout }) {
   return (
-    <section className={scss.container}>
+    <section className={scss.container} id='about'>
       <Container>
-        {/* {about.image ?? <img src={about.image.url} alt={about.image.alt} />}
-        <Title order={3}>{about.title}</Title>
-        {about.description && <p>{about.description}</p>}
-        {about.links?.map(link) => (
-          <Link src={about.}></Link>
-        )} */}
+        {about.image.url ? (
+          <img src={about.image.url} alt={about.image.alt} />
+        ) : (
+          <></>
+        )}
+        <Box className={scss.text}>
+          <Title order={3}>{about.title}</Title>
+          <Text>{about.descriptionFirstP && about.descriptionFirstP}</Text>
+          <Text>{about.descriptionSecondP && about.descriptionSecondP}</Text>
+        </Box>
       </Container>
     </section>
   );
