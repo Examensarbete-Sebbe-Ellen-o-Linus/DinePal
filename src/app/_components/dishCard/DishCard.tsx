7bd7f70d277d44c0715b27f38cd333b554220d52
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-01-08 17:24:13 +0100

    Update selectedDishes with slug and add link in DishCard

--- src/app/_components/dishCard/DishCard.tsx
+++ src/app/_components/dishCard/DishCard.tsx
@@ -1,5 +1,6 @@
 import { Box, Divider, Text, Title } from '@mantine/core';
 
+import Link from 'next/link';
 import type { IDish } from '~/app/interfaces';
 import LongButton from '../longButton/LongButton';
 import classes from './DishCard.module.css';
@@ -31,21 +32,25 @@ const getTagIconComponent = (tag: string) => {
 };
 
 export default function DishCard({ showDescription, dish }: Props) {
+  const dishLink = `/product/${dish?.slug?.current ?? '404'}`;
+
+  console.log(dish);
   return (
     <Box className={classes.card}>
-      <img
-        className={classes.image}
-        src={dish.image.url}
-        alt={dish.image.alt}
-      />
-
+      <Link href={dishLink}>
+        <img
+          className={classes.image}
+          src={dish.image.url}
+          alt={dish.image.alt}
+        />
+      </Link>
       <Box className={classes.headingPrice}>
-        <Title order={6}>{dish.title}</Title>
+        <Link href={dishLink}>
+          <Title order={6}>{dish.title}</Title>
+        </Link>
         <Text>{dish.price} SEK</Text>
       </Box>
-
       {showDescription && <Text>{dish.description}</Text>}
-
       <Box className={classes.iconContainer}>
         <Box className={classes.iconContainer}>
           {dish.tags.map((tag, i) => {
