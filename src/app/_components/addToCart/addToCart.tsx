7b9c1ea836f35b00e7d2eae9e2fe8496e211d897
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-01-09 18:34:06 +0100

    Update add and remove from cart buttons with svg and style

--- src/app/_components/addToCart/addToCart.tsx
+++ src/app/_components/addToCart/addToCart.tsx
@@ -1,7 +1,8 @@
-import { Text } from '@mantine/core';
+'use client';
+
 import { useCart } from 'context/cartContext';
 import type { IDish } from '~/app/interfaces';
-import classes from './addToCart.module.css';
+import scss from './addToCart.module.scss';
 
 type CartItem = {
   dish: IDish;
@@ -40,12 +41,22 @@ export const AddToCartButton = ({ dish }: Props) => {
 
   return (
     <>
-      <Text
-        className={classes.cartButton}
-        onClick={() => handleAddToCart(dish)}
-      >
-        +
-      </Text>
+      <button className={scss.cartButton} onClick={() => handleAddToCart(dish)}>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='7'
+          height='5'
+          viewBox='0 0 7 5'
+          fill='none'
+        >
+          <path
+            fillRule='evenodd'
+            clipRule='evenodd'
+            d='M3.17575 0.801235C3.35474 0.622245 3.64494 0.622245 3.82393 0.801235L6.57393 3.55123C6.75292 3.73023 6.75292 4.02043 6.57393 4.19942C6.39494 4.37841 6.10474 4.37841 5.92575 4.19942L3.49984 1.77351L1.07393 4.19942C0.894938 4.37841 0.604737 4.37841 0.425747 4.19942C0.246756 4.02043 0.246756 3.73023 0.425747 3.55123L3.17575 0.801235Z'
+            fill='#F5F5F1'
+          />
+        </svg>
+      </button>
     </>
   );
 };
