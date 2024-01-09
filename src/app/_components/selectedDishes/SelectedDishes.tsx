fed8d0b64b21f23403e4a6d324df302d5bf9f2bb
sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2024-01-09 13:56:45 +0100

    cleanup

--- src/app/_components/selectedDishes/SelectedDishes.tsx
+++ src/app/_components/selectedDishes/SelectedDishes.tsx
@@ -3,17 +3,9 @@
 import Link from 'next/link';
 
 import { Text } from '@mantine/core';
-import { useCart } from 'context/cartContext';
-import { useEffect } from 'react';
 import type { IDish } from '../../interfaces';
-import { AddToCartButton } from '../addToCart/addToCart';
-import { RemoveFromCartButton } from '../removeFromCart/removeFromCart';
 
 export default function SelectedDishes({ dishes }: { dishes: IDish[] }) {
-  const { cartState, setCartState } = useCart();
-  useEffect(() => {
-    console.log('cartState', cartState);
-  }, [cartState]);
   return (
     <section>
       <h3>Example how to display selected Dishes</h3>
@@ -30,15 +22,6 @@ export default function SelectedDishes({ dishes }: { dishes: IDish[] }) {
                 </span>
               ))}
 
-            {cartState.map((item, i) => (
-              <div key={i}>
-                <span style={{ fontWeight: 'bold' }}>{item.dish.title}</span>
-                <span style={{ fontWeight: 'bold' }}>x {item.quantity}</span>
-              </div>
-            ))}
-
-            <AddToCartButton dish={dish} />
-            <RemoveFromCartButton dish={dish} />
             <Text> pris: {dish.price} :-</Text>
 
             {dish.slug && dish.slug.current && (
