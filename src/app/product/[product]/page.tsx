cf3d2e0a5f2c4f049f47d86a5ac6e317b5aacc17
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-01-09 11:55:34 +0100

    Update content and style of single dish page

--- src/app/product/[product]/page.tsx
+++ src/app/product/[product]/page.tsx
@@ -1,6 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  Box,
+  Container,
+  Divider,
+  NumberInput,
+  Text,
+  Title,
+  Tooltip,
+} from '@mantine/core';
+import { IconKey, tagDetails } from '~/app/_components/dishCard/DishCard';
+import LongButton from '~/app/_components/longButton/LongButton';
 import { fetchSingleDish } from '~/server/sanity/sanity.utils';
+import scss from './page.module.scss';
 
 interface Props {
   params: {
@@ -13,8 +25,47 @@ export default async function Product({ params }: Props) {
   const dish = await fetchSingleDish(slug);
 
   return (
-    <>
-      <h1>{dish.title}</h1>
-    </>
+    <Container className={scss.container}>
+      <Box className={scss.card}>
+        <img src={dish.image.url} alt={dish.image.alt} />
+        <Box className={scss.text}>
+          <Box>
+            <Box className={scss.top}>
+              <Title order={2}>{dish.title}</Title>
+              <Title order={3}>{dish.price} :-</Title>
+            </Box>
+            <Box className={scss.middle}>
+              <Text>{dish.description}</Text>
+              <Box className={scss.iconContainer}>
+                {dish.tags.map((tagValue, i) => {
+                  const tagInfo = tagDetails[tagValue as IconKey];
+                  if (!tagInfo) return null;
+                  const { title, Icon } = tagInfo;
+                  return (
+                    <Tooltip label={title} key={i}>
+                      <Box>
+                        <Icon />
+                      </Box>
+                    </Tooltip>
+                  );
+                })}
+              </Box>
+            </Box>
+            <Divider />
+          </Box>
+          <Box className={scss.bottom}>
+            <NumberInput
+              className={scss.input}
+              defaultValue={1}
+              min={1}
+              max={100}
+              size={'xs'}
+              allowDecimal={false}
+            />
+            <LongButton showAddIcon={true} text={'Lägg till'} color={'black'} />
+          </Box>
+        </Box>
+      </Box>
+    </Container>
   );
 }
