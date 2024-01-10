6523b1027b4d9f0231148ae9c644f7dfb960f875
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-01-10 10:02:06 +0100

    Update sigle dish page with quantity button

--- src/app/product/[product]/page.tsx
+++ src/app/product/[product]/page.tsx
@@ -1,17 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import {
-  Box,
-  Container,
-  Divider,
-  NumberInput,
-  Text,
-  Title,
-  Tooltip,
-} from '@mantine/core';
+'use client';
+
+import { Box, Container, Divider, Text, Title, Tooltip } from '@mantine/core';
+import { useCart } from 'context/cartContext';
+import { useEffect, useState } from 'react';
 import AddButton from '~/app/_components/addButton/AddButton';
-import { IconKey, tagDetails } from '~/app/_components/dishCard/DishCard';
+import { tagDetails, type IconKey } from '~/app/_components/dishCard/DishCard';
+import { type IDish } from '~/app/interfaces';
 import { fetchSingleDish } from '~/server/sanity/sanity.utils';
+import QuantityButton from '../../_components/quantityButton/QuantityButton';
 import scss from './page.module.scss';
 
 interface Props {
@@ -20,52 +16,73 @@ interface Props {
   };
 }
 
-export default async function Product({ params }: Props) {
+export default function Product({ params }: Props) {
   const slug = params.product;
-  const dish = await fetchSingleDish(slug);
+  const { handleAddToCart } = useCart();
+  const [quantity, setQuantity] = useState(1);
+  const [dish, setDish] = useState<IDish | null>(null);
+  const [error, setError] = useState();
 
-  return (
-    <Container className={scss.container}>
-      <Box className={scss.card}>
-        <img src={dish.image.url} alt={dish.image.alt} />
-        <Box className={scss.text}>
-          <Box>
-            <Box className={scss.top}>
-              <Title order={2}>{dish.title}</Title>
-              <Title order={3}>{dish.price} :-</Title>
-            </Box>
-            <Box className={scss.middle}>
-              <Text>{dish.description}</Text>
-              <Box className={scss.iconContainer}>
-                {dish.tags.map((tagValue, i) => {
-                  const tagInfo = tagDetails[tagValue as IconKey];
-                  if (!tagInfo) return null;
-                  const { title, Icon } = tagInfo;
-                  return (
-                    <Tooltip label={title} key={i}>
-                      <Box>
-                        <Icon />
-                      </Box>
-                    </Tooltip>
-                  );
-                })}
+  useEffect(() => {
+    const loadDish = async () => {
+      try {
+        const fetchedDish = await fetchSingleDish(slug);
+        setDish(fetchedDish);
+      } catch (err) {
+        setError(error);
+      }
+    };
+
+    void loadDish();
+  }, [error, slug]);
+
+  if (dish) {
+    const addToCart = (quantity: number) => {
+      handleAddToCart(dish, quantity);
+      setQuantity(1);
+    };
+
+    return (
+      <Container className={scss.container}>
+        <Box className={scss.card}>
+          <img src={dish.image.url} alt={dish.image.alt} />
+          <Box className={scss.text}>
+            <Box>
+              <Box className={scss.top}>
+                <Title order={2}>{dish.title}</Title>
+                <Title order={3}>{dish.price} :-</Title>
               </Box>
+              <Box className={scss.middle}>
+                <Text>{dish.description}</Text>
+                <Box className={scss.iconContainer}>
+                  {dish.tags.map((tagValue, i) => {
+                    const tagInfo = tagDetails[tagValue as IconKey];
+                    if (!tagInfo) return null;
+                    const { title, Icon } = tagInfo;
+                    return (
+                      <Tooltip label={title} key={i}>
+                        <Box>
+                          <Icon />
+                        </Box>
+                      </Tooltip>
+                    );
+                  })}
+                </Box>
+              </Box>
+              <Divider />
             </Box>
-            <Divider />
-          </Box>
-          <Box className={scss.bottom}>
-            <NumberInput
-              className={scss.input}
-              defaultValue={1}
-              min={1}
-              max={100}
-              size={'xs'}
-              allowDecimal={false}
-            />
-            <AddButton showAddIcon={true} text={'Lägg till'} color={'black'} />
+            <Box className={scss.bottom}>
+              <QuantityButton quantity={quantity} setQuantity={setQuantity} />
+              <AddButton
+                showAddIcon={true}
+                text={'Lägg till'}
+                color={'black'}
+                onClick={() => addToCart(quantity)}
+              />
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Container>
-  );
+      </Container>
+    );
+  }
 }
