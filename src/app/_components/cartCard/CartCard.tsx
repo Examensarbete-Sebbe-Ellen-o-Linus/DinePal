7f7bd5d459ac45f69270e647cc54ef05114ae40b
Linus Hernvall <linus.hernvall@gmail.com>
2024-01-11 19:18:41 +0100

    FIxed mapping in Cart. Select and remove on CheckoutCard.

--- src/app/_components/cartCard/CartCard.tsx
+++ src/app/_components/cartCard/CartCard.tsx
@@ -1,35 +1,34 @@
 import { Box, Divider, Text, Title } from '@mantine/core';
 
 import { useCart } from 'context/cartContext';
-import React from 'react';
+import { type CartItem } from 'context/initializers';
 import Quantity from '../quantityButton/QuantityButton';
 import { RemoveFromCartButton } from '../removeFromCart/removeFromCart';
 import classes from './CartCard.module.scss';
 
-export default function CartCard() {
-  const { cartState, updateItemQuantity } = useCart();
+interface ICartCard {
+  item: CartItem;
+}
 
+export default function CartCard({ item }: ICartCard) {
+  const { updateItemQuantity } = useCart();
   return (
     <Box className={classes.container}>
-      {cartState.map((item, i) => (
-        <React.Fragment key={i}>
-          <img src={item.dish.image.url} alt={item.dish.image.alt} />
-          <Title order={6}>{item.dish.title}</Title>
-          <Text>{item.dish.price} :-</Text>
-          <Divider mt={0} mb={0} w={'100%'} my='md' />
-          <Box className={classes.selectRemove}>
-            <Quantity
-              quantity={item.quantity}
-              setQuantity={newQuantity =>
-                updateItemQuantity(item.dish, newQuantity)
-              }
-            />
-            <Box>
-              <RemoveFromCartButton dish={item.dish} />
-            </Box>
-          </Box>
-        </React.Fragment>
-      ))}
+      <img src={item.dish.image.url} alt={item.dish.image.alt} />
+      <Title order={6}>{item.dish.title}</Title>
+      <Text>{item.dish.price} :-</Text>
+      <Divider mt={0} mb={0} w={'100%'} my='md' />
+      <Box className={classes.selectRemove}>
+        <Quantity
+          quantity={item.quantity}
+          setQuantity={newQuantity =>
+            updateItemQuantity(item.dish, newQuantity)
+          }
+        />
+        <Box>
+          <RemoveFromCartButton dish={item.dish} />
+        </Box>
+      </Box>
     </Box>
   );
 }
