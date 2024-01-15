03394891d73b6df0ed4e40e38a6f929c307516d0
Linus Hernvall <linus.hernvall@gmail.com>
2024-01-15 17:29:47 +0100

    Hero position

--- src/app/_components/hero/Hero.tsx
+++ src/app/_components/hero/Hero.tsx
@@ -1,4 +1,4 @@
-import { Container, Title } from '@mantine/core';
+import { Box, Title } from '@mantine/core';
 import Link from 'next/link';
 import { type IHero } from '../../interfaces';
 import ShortButton from '../shortButton/ShortButton';
@@ -6,20 +6,33 @@ import classes from './Hero.module.scss';
 
 export default async function Hero({ hero }: { hero: IHero }) {
   return (
-    <Container className={classes.hero}>
-      <Title order={1} className={classes.title}>
-        {hero.title}
-      </Title>
+    <Box className={classes.container}>
       {hero.description && <p>{hero.description}</p>}
-      {hero.image && <img src={hero.image.url} alt={hero.image.alt}></img>}
-      {hero.buttons &&
-        hero.buttons.map((button, i) => <button key={i}>{button.text}</button>)}
-      <Link href='/menu'>
-        <ShortButton text={'Meny'} color={'orange'} />
-      </Link>
-      <Link href='/booking'>
-        <ShortButton text={'Boka bord'} color={'black'} />
-      </Link>
-    </Container>
+      <Box className={classes.overlay} />
+      {hero.image && (
+        <img
+          className={classes.heroImage}
+          src={hero.image.url}
+          alt={hero.image.alt}
+        />
+      )}
+      <Box className={classes.content}>
+        {hero.buttons &&
+          hero.buttons.map((button, i) => (
+            <button key={i}>{button.text}</button>
+          ))}
+        <Title order={1} className={classes.title}>
+          {hero.title}
+        </Title>
+        <Box className={classes.buttonContainer}>
+          <Link href='/menu'>
+            <ShortButton text={'Meny'} color={'orange'} />
+          </Link>
+          <Link href='/booking'>
+            <ShortButton text={'Boka bord'} color={'black'} />
+          </Link>
+        </Box>
+      </Box>
+    </Box>
   );
 }
