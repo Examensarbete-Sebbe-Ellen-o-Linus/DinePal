63fe2cc0bdb2a33c11d74f0c7741ac6b343e4cd7
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-01-19 12:20:24 +0100

    Create menuDishCard wich differs from dishCard by container description but not link or hover

---
+++ src/app/_components/menuDishCard/MenuDishCard.tsx
@@ -0,0 +1,62 @@
+import { Box, Divider, Text, Title } from '@mantine/core';
+import { useCart } from 'context/cartContext';
+import type { IDish } from '~/app/interfaces';
+import AddButton from '../addButton/AddButton';
+import PlaceholderSmall from '../placeholderSmall/PlaceholderSmall';
+import Tags from '../tags/Tags';
+import classes from './MenuDishCard.module.scss';
+
+interface Props {
+  showDescription: boolean;
+  dish: IDish;
+}
+
+export default function MenuDishCard({ showDescription, dish }: Props) {
+  const { handleAddToCart } = useCart();
+
+  return (
+    <Box className={classes.card} id={dish.slug.current}>
+      {dish.image.url ? (
+        <Box>
+          <img
+            className={classes.image}
+            src={dish.image.url}
+            alt={dish.image.alt}
+          />
+        </Box>
+      ) : (
+        <PlaceholderSmall />
+      )}
+
+      <Box className={classes.top}>
+        <Box className={classes.textTop}>
+          <Box className={classes.headingPrice}>
+            <Title order={6}>{dish.title}</Title>
+            <Text>{dish.price}:-</Text>
+          </Box>
+          {dish.description && <Text>{dish.description}</Text>}
+        </Box>
+        <Box className={classes.bottom}>
+          <Box className={classes.iconContainer}>
+            <Box className={classes.iconContainer}>
+              <Tags dish={dish} />
+            </Box>
+          </Box>
+          <Divider
+            mt={0}
+            mb={16}
+            w={'100%'}
+            my='md'
+            className={classes.bottom}
+          />
+          <AddButton
+            showAddIcon={true}
+            text={'Lägg till'}
+            color={'black'}
+            onClick={() => handleAddToCart(dish)}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
