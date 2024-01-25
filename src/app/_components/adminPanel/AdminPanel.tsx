2315ee03bc4a77e0b548d808bed81c54c5c6ff1a
sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2024-01-25 11:45:44 +0100

    added notifications to adminPanel

--- src/app/_components/adminPanel/AdminPanel.tsx
+++ src/app/_components/adminPanel/AdminPanel.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { Box } from '@mantine/core';
+import { showNotification } from '@mantine/notifications';
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
+import { theme } from '~/app/_theme/theme';
 import { api } from '~/trpc/react';
 import Bookings from '../bookings/Bookings';
 import Orders from '../orders/Orders';
@@ -15,13 +17,30 @@ export default function AdminPanel() {
   const [render, setRender] = useState<RenderSection>('orders');
 
   const { refetch: refetchBookings } = api.booking.getTableBookings.useQuery();
+  const { refetch: refetchOrders } = api.order.getTodaysOrders.useQuery();
 
   useEffect(() => {
     const fetchNewBookings = async () => {
       await refetchBookings();
       console.log('useEffect triggered by socket!');
     };
+    const fetchNewOrders = async () => {
+      await refetchOrders();
+      console.log('useEffect triggered by socket!');
+    };
     socket.on('bookingCreated', fetchNewBookings);
+    showNotification({
+      title: 'Ny bokning inkommen!',
+      message: 'Se den nya bokningen under boknings fliken.',
+      color: theme.colors?.orange ? theme.colors.orange[3] : '#FF5B00',
+    });
+
+    socket.on('orderCreated', fetchNewOrders);
+    showNotification({
+      title: 'Ny order inkommen!',
+      message: 'Se den nya ordern under order fliken',
+      color: theme.colors?.orange ? theme.colors.orange[3] : '#FF5B00',
+    });
 
     return () => {
       socket.off('bookingCreated', fetchNewBookings);
